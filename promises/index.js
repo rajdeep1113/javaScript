04dc2a709effee163715a8284489979a758463c8
rajdeep1113 <rajdeepsing7568@gmail.com>
2024-12-30 18:02:05 +0530

    promises

---
+++ promises/index.js
@@ -0,0 +1,17 @@
+let promise = new Promise((resolve, reject) => {
+    if (12 % 2 == 0) {
+        return resolve("even")
+    } else {
+        return reject("odd")
+    }
+})
+
+
+promise
+    .then((data) => {
+        console.log(data,"resolve")
+    })
+    .catch((err) => {
+        console.log(err,"error");
+
+    })
